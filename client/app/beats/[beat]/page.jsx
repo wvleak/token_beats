a3ee5351b3fa61b680629ca088eea4c2469caee4
Teouri Maleak <73338115+wvleak@users.noreply.github.com>
2023-07-06 11:13:17 +0200

    beat page

--- client/app/beats/[beat]/page.jsx
+++ client/app/beats/[beat]/page.jsx
@@ -19,11 +19,30 @@ const BeatDetails = ({ params }) => {
 
   //console.log(JSON.parse(router.query.beat));
   return (
-    <div>
-      <input type="text" placeholder="beatId" />
-      <button className="bg-white" type="submit" onClick={handleSubmit}>
-        Buy
-      </button>
+    <div className="flex flex-col gap-8 mt-[90px]">
+      <div className="flex flex-row  gap-5 md:w-[800px] lg:w-[1000px]">
+        <div className="w-[288px]">
+          <img src="https://www.billboard.com/wp-content/uploads/media/kanye-west-the-life-of-pablo-album-2016-billboard-1024.jpg?w=1024" />
+        </div>
+        <div className="flex flex-col ml-10 gap-5">
+          <h1 className="text-white">Title</h1>
+          <h1 className="text-white">Producer</h1>
+          <h1 className="text-white">Supply left</h1>
+          <h1 className="text-white">Price</h1>
+          <h1 className="text-white">Tags</h1>
+        </div>
+      </div>
+      <div className="flex flex-col gap-5">
+        <p className="text-white">Player</p>
+
+        <button
+          className="bg-white max-w-[288px]"
+          type="submit"
+          onClick={handleSubmit}
+        >
+          Buy
+        </button>
+      </div>
     </div>
   );
 };
